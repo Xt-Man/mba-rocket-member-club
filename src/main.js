5f560b559369151851f2a602f65b7c0562d73444
Edgar Bottini <edgarbottini@hotmail.com>
2024-07-30 05:30:18 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// Configuração do dayjs
+import "./libs/dayjs.js"
+
+//CSS
+import "./styles/global.css"
+import "./styles/index.css"
+import "./styles/header.css"
+import "./styles/form.css"
+import "./styles/main.css"
+
+import dayjs from "dayjs"
+console.log(dayjs().format("HH:mm"))
